6aefee6f1e24e7e9d5fcb4aa801c614073a32f03
crimson666 <ankurbsws@gmail.com>
2020-08-09 03:44:36 +0530

    UI part Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NoteContainerComponent } from './note-container/note-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NoteContainerComponent
   ],
   imports: [
     BrowserModule,
