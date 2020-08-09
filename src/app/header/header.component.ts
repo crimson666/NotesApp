afd86825ad4bbfc08c26a3087bf6006b1b001a35
crimson666 <ankurbsws@gmail.com>
2020-08-10 02:23:59 +0530

    Data Stuff

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
+  testform = new FormGroup({
+    lable: new FormControl(''),
+    textBody: new FormControl(''),
+  });
+  SearchForm = new FormGroup({
+    search: new FormControl('')
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  Submit(testform){
+    console.warn(this.testform.value);
+    localStorage.setItem('NotesData', JSON.stringify(this.testform.value));
+  }
+  SearchResult(SearchForm){
+    console.warn(this.SearchForm.value);
   }
 
 }
--- src/app/note-container/note-container.component.ts
+++ src/app/note-container/note-container.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-container.component.css']
 })
 export class NoteContainerComponent implements OnInit {
-
+  formValue: any = [];
   constructor() { }
 
+  NotesAppsList = [
+    {
+      "lable": "My Frist Text",
+      "textBody": "Frist Bla bla bla bla"
+    },
+    {
+      "lable": "My Frist Text",
+      "textBody": "Frist Bla bla bla bla"
+    },
+    {
+      "lable": "My Frist Text",
+      "textBody": "Frist Bla bla bla bla"
+    },
+    {
+      "lable": "My Frist Text",
+      "textBody": "Frist Bla bla bla bla"
+    },
+    {
+      "lable": "My Frist Text",
+      "textBody": "Frist Bla bla bla bla"
+    },
+    {
+      "lable": "My Frist Text",
+      "textBody": "Frist Bla bla bla bla"
+    },
+    {
+      "lable": "My Frist Text",
+      "textBody": "Frist Bla bla bla bla"
+    },
+    {
+      "lable": "My Frist Text",
+      "textBody": "Frist Bla bla bla bla"
+    },
+  ];
+
   ngOnInit(): void {
+    //this.formValue = JSON.parse(localStorage.getItem('NotesData'));
+    this.getNoteList();
   }
+  getNoteList(){
+    this.formValue = this.NotesAppsList;
+  }
+  
+
 
 }
